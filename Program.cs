29b9ccf9f84527705110800c37632168af9c743b
kadirbeskardes <73891186+kadirbeskardes@users.noreply.github.com>
2023-05-23 18:15:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Answer11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> words = new Stack<string>();
+            string input= "The weather is so sunny nowadays\nLife is so beautiful";
+            StringBuilder word=new StringBuilder();
+            StringBuilder output=new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]==' ')
+                {
+                    words.Push(word.ToString());
+                    word.Clear();
+                }
+                else if (input[i] == '\n')
+                {
+                    words.Push(word.ToString());
+                    word.Clear();
+                    while (words.Count > 0)
+                    {
+                        output.Append(words.Pop() + ' ');
+                    }
+                    output.AppendLine();
+                }
+                else
+                {
+                    word.Append(input[i]);
+                }
+            }
+            if (word.Length>0)
+            {
+                words.Push(word.ToString());
+                word.Clear();
+            }
+            while (words.Count > 0)
+            {
+                output.Append(words.Pop() + ' ');
+            }
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
